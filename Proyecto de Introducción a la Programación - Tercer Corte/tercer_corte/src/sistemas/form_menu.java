9f289c5e7c6918b2a4f8597dc2b7d6fb451ebf14
Juan Ricardo Torres <62718487+JuanR1cardo@users.noreply.github.com>
2020-06-18 16:31:11 -0500

    Add files via upload

---
+++ Proyecto de Introducción a la Programación - Tercer Corte/tercer_corte/src/sistemas/form_menu.java
@@ -0,0 +1,39 @@
+package sistemas;
+/*Author: Juan Ricardo Torres Céspedes
+	  Date: 21/04/2020
+	  Description: This software calculates determined values for N products
+	 */
+import javax.swing.*;
+import javax.swing.event.AncestorListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class form_menu extends JFrame {
+    public form_menu() {
+        JLabel label_menu = new JLabel("version JAVA - SAC", SwingConstants.CENTER);
+        label_menu.setBounds(200, 10, 200, 50);
+        add(label_menu);
+        JLabel label_maker = new JLabel("Creado por: Juan Ricardo Torres Céspedes", SwingConstants.LEFT);
+        label_maker.setBounds(5, 300, 500, 20);
+        add(label_maker);
+        JButton boton_iniciar = new JButton("INICIAR");
+        boton_iniciar.setBounds(250, 150, 120, 40);
+        boton_iniciar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed (ActionEvent e) {
+                form_principal form_principal1 = new form_principal();
+                setVisible(false);
+            }
+        });
+        add(boton_iniciar);
+        setLayout(null);
+        setVisible(true);
+        setResizable(false);
+        setBounds(0,0,600,400);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+}
+
+
